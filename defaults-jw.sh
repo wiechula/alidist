9aad9cd21880f5bf2812bc4336116cd88909c59e
wiechula <Jens.Wiechula@cern.ch>
2017-06-20 09:29:27 +0200

    sync to latest master

--- defaults-jw.sh
+++ defaults-jw.sh
@@ -1,13 +1,24 @@
 package: defaults-jw
 version: v1
 env:
-  CXXFLAGS: "-fPIC -g -O2 -std=c++11"
+  CXXFLAGS: "-fPIC -g -O2 -std=c++14"
   CFLAGS: "-fPIC -g -O2"
   CMAKE_BUILD_TYPE: "RELWITHDEBINFO"
 overrides:
+  autotools:
+    tag: v1.5.0
+  boost:
+    requires:
+      - "GCC-Toolchain:(?!osx)"
+      - Python
+  GCC-Toolchain:
+    tag: v6.2.0-alice1
+    prefer_system_check: |
+      set -e
+      which gfortran || { echo "gfortran missing"; exit 1; }
+      which cc && test -f $(dirname $(which cc))/c++ && printf "#define GCCVER ((__GNUC__ << 16)+(__GNUC_MINOR__ << 8)+(__GNUC_PATCHLEVEL__))\n#if (GCCVER < 0x060200)\n#error \"System's GCC cannot be used: we need at least GCC 6.X. We are going to compile our own version.\"\n#endif\n" | cc -xc++ - -c -o /dev/null
   ROOT:
-    version: "%(tag_basename)s"
-    tag: "v6-08-06"
+    tag: "v6-08-02"
     source: https://github.com/root-mirror/root
     requires:
       - AliEn-Runtime:(?!.*ppc64)
@@ -16,15 +27,20 @@ overrides:
       - Xdevel:(?!osx)
       - FreeType:(?!osx)
       - Python-modules
+  AliRoot:
+    requires:
+      - ROOT
+      - fastjet:(?!.*ppc64)
+      - GEANT3
+      - GEANT4_VMC
+      - Vc
+      - ZeroMQ
   GSL:
     prefer_system_check: |
       printf "#include \"gsl/gsl_version.h\"\n#define GSL_V GSL_MAJOR_VERSION * 100 + GSL_MINOR_VERSION\n# if (GSL_V < 116)\n#error \"Cannot use system's gsl. Notice we only support versions from 1.16 (included)\"\n#endif\nint main(){}" | gcc  -I$(brew --prefix gsl)/include -xc++ - -o /dev/null
   protobuf:
     version: "%(tag_basename)s"
     tag: "v3.0.2"
-  libpng:
-  Python-modules:
-  Python:
   CMake:
     version: "%(tag_basename)s"
     tag: "v3.7.2"
