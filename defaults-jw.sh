662a44e0ee9a352a594eff95e47f20e2761a164c
wiechula <Jens.Wiechula@cern.ch>
2018-01-15 11:02:26 +0100

    Sync with origin/master

--- defaults-jw.sh
+++ defaults-jw.sh
@@ -4,6 +4,10 @@ env:
   CXXFLAGS: "-fPIC -g -O2 -std=c++14"
   CFLAGS: "-fPIC -g -O2"
   CMAKE_BUILD_TYPE: "RELWITHDEBINFO"
+  CXXSTD: "14"
+disable:
+  - grpc
+  - Ppconsul
 overrides:
   autotools:
     tag: v1.5.0
@@ -11,7 +15,7 @@ overrides:
     tag: "v1.64.0-alice1"
     requires:
       - "GCC-Toolchain:(?!osx)"
-      - Python
+      - Python-modules
     prefer_system_check: |
       printf "#include \"boost/version.hpp\"\n# if (BOOST_VERSION < 106400 || BOOST_VERSION > 106499)\n#error \"Cannot use system's boost: boost 1.64 required.\"\n#endif\nint main(){}" | gcc -I$(brew --prefix boost)/include -xc++ - -o /dev/null
   GCC-Toolchain:
@@ -21,8 +25,8 @@ overrides:
       which gfortran || { echo "gfortran missing"; exit 1; }
       which cc && test -f $(dirname $(which cc))/c++ && printf "#define GCCVER ((__GNUC__ << 16)+(__GNUC_MINOR__ << 8)+(__GNUC_PATCHLEVEL__))\n#if (GCCVER < 0x060200)\n#error \"System's GCC cannot be used: we need at least GCC 6.X. We are going to compile our own version.\"\n#endif\n" | cc -xc++ - -c -o /dev/null
   ROOT:
-    version: "v6-10-06+git_%(short_hash)s"
-    tag: "c54db1c10b19b05f157dc44078b45edd9f38c741"
+    version: "%(tag_basename)s"
+    tag: "v6-10-08"
     source: https://github.com/root-mirror/root
     requires:
       - AliEn-Runtime:(?!.*ppc64)
@@ -32,7 +36,10 @@ overrides:
       - FreeType:(?!osx)
       - Python-modules
       - "GCC-Toolchain:(?!osx)"
+      - libpng
+      - lzma
   AliRoot:
+    version: "%(commit_hash)s_O2"
     requires:
       - ROOT
       - fastjet:(?!.*ppc64)
@@ -51,6 +58,23 @@ overrides:
     tag: "v3.9.4"
     prefer_system_check: |
       which cmake && case `cmake --version | sed -e 's/.* //' | cut -d. -f1,2,3 | head -n1` in [0-2]*|3.[0-8].*|3.9.[0-3]) exit 1 ;; esac
+  pythia:
+    requires:
+      - lhapdf
+      - boost
+  AliPhysics:
+    version: "%(commit_hash)s_O2"
+  GEANT4:
+    tag: v10.3.3
+    source: https://gitlab.cern.ch/geant4/geant4.git
+  GEANT4_VMC:
+    tag: "v3-5"
+    source: https://github.com/vmc-project/geant4_vmc
+  vgm:
+    tag: "v4-4"
+    source: https://github.com/vmc-project/vgm
+  GEANT3:
+    tag: v2-5
 ---
 # This file is included in any build recipe and it's only used to set
 # environment variables. Which file to actually include can be defined by the
